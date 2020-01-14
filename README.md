73e7dbec19f3f7ea1c6490a24233ddd593334b97
andyanguiano <aanguiano@chapman.edu>
2020-01-14 21:00:31 +0000

    I made changes

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # int2020cpsc298
+
+Andy Anguiano
+#2316199
+Assignment x
+
+this is the helloWorld Assignemnt
+
+I did not use any online resources 
--- helloWorldTest.cpp
+++ helloWorldTest.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main() {
   cout << "Hello World" << endl;
+  cout << " I had to add to make this fix"<< endl;
 
   return 0;
 }
